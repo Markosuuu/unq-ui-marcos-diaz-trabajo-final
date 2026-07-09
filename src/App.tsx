f83630ce1929971711085843fffd3a6c631e403b
Marcos Diaz <ma.nahuel.d@gmail.com>
2026-07-09 02:36:34 -0300

    feat: se añade el timer y termina la partida

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { validarPalabra } from "./services/palabrasService";
 
@@ -6,6 +6,26 @@ const App = () => {
   const [palabra, setPalabra] = useState<string>("");
   const [listaPalabras, setListaPalabras] = useState<string[]>([]);
   const [error, setError] = useState<string | null>(null);
+
+  const [segundos, setSegundos] = useState<number>(15);
+  const [mensaje, setMensaje] = useState<string>("");
+  const [isActivo, setIsActivo] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isActivo) return;
+
+    if (segundos === 0) {
+      setIsActivo(false);
+      setMensaje("Partida finalizada");
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setSegundos((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [isActivo, segundos]); // TODO: Botón para iniciar devuelta el contador
 
   const handlePalabra = async (palabra: string) => {
     setPalabra("");
@@ -16,25 +36,42 @@ const App = () => {
       return setError(resultado.error || "Error desconocido");
     }
 
+    if (listaPalabras.length === 0) {
+      setSegundos(15);
+      setMensaje("");
+      setIsActivo(true);
+    }
+
     setListaPalabras([...listaPalabras, resultado.palabra || ""]);
     setError(null);
   };
 
   return (
     <>
+      <div className="timer">
+        {segundos !== 0 ? (
+          <span>{segundos}</span>
+        ) : (
+          <span>Tiempo finalizado</span>
+        )}
+      </div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handlePalabra(palabra);
         }}
       >
-        <input
-          type="text"
-          placeholder="Ingrese una palabra..."
-          value={palabra}
-          onChange={(e) => setPalabra(e.target.value)}
-        />
-        <button type="submit">Enviar</button>
+        {segundos !== 0 && (
+          <>
+            <input
+              type="text"
+              placeholder="Ingrese una palabra..."
+              value={palabra}
+              onChange={(e) => setPalabra(e.target.value)}
+            />
+            <button type="submit">Enviar</button>
+          </>
+        )}
       </form>
       {error && <p style={{ color: "red" }}>{error}</p>}
 
