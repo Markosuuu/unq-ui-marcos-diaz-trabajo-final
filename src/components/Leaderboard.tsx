8413ee3344256075a40adb04c5b199044d11b1f7
Marcos Diaz <ma.nahuel.d@gmail.com>
2026-07-15 15:46:53 -0300

    style: se estiliza la página de puntaje

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -6,23 +6,33 @@ type Props = {
 };
 
 const Leaderboard = ({ leaderBoard }: Props) => {
+  const elegirMoneda = (n: number) => {
+    switch (true) {
+      case 0 == n:
+        return "https://static.wikia.nocookie.net/terraria_gamepedia/images/f/f8/Platinum_Coin.gif/revision/latest?cb=20150713204825&format=original";
+      case n < 4:
+        return "https://static.wikia.nocookie.net/terraria_gamepedia/images/b/b0/Gold_Coin.gif/revision/latest?cb=20150713204755&format=original";
+      case n < 7:
+        return "https://static.wikia.nocookie.net/terraria_gamepedia/images/c/cf/Silver_Coin.gif/revision/latest?cb=20150713204034&format=original";
+      default:
+        return "https://static.wikia.nocookie.net/terraria_gamepedia/images/8/8f/Copper_Coin.gif/revision/latest?cb=20150703190535&format=original";
+    }
+  };
+
   return (
     <>
       <h2 className={style["titulo"]}>Leaderboard</h2>
-      <table className={style["tablero-puntajes"]}>
-        <tr className={style["cabecera-leaderboard"]}>
-          <td>Puesto</td>
-          <td>Nombre</td>
-          <td>Puntos</td>
-        </tr>
+      <ul className={style["lista-puntajes"]}>
         {leaderBoard.map((item: LeaderBoardItem, index: number) => (
-          <tr key={index}>
-            <td>{"N°" + (index + 1)}</td>
-            <td>{item.nombre}</td>
-            <td>{item.puntaje}</td>
-          </tr>
+          <li key={index}>
+            <div className={style["nombres-leaderboard"]}>
+              <img src={elegirMoneda(index)} alt="" />
+              <span>{item.nombre}</span>
+            </div>
+            <span className={style["puntaje-leaderboard"]}>{item.puntaje}</span>
+          </li>
         ))}
-      </table>
+      </ul>
     </>
   );
 };
--- src/pages/ScoreView.tsx
+++ src/pages/ScoreView.tsx
@@ -21,18 +21,34 @@ const ScoreView = () => {
   return (
     <main>
       <article className={style["score-container"]}>
-        <section className={style["section-puntaje"]}>
-          <h1>Tiempo finalizado</h1>
-          <span>Tu puntaje final fue: {puntaje}</span>
-          <Link to={"/"} className={style["btn-reiniciar"]}>
-            Otra partida
-          </Link>
-        </section>
+        <h1 className={style["titulo"]}>Palabras encadenadas</h1>
         {leaderBoard.length != 0 && (
           <aside className={style["leaderBoard"]}>
             <Leaderboard leaderBoard={leaderBoard} />
           </aside>
         )}
+        <section className={style["section-puntaje"]}>
+          <div className={style["estadisticas"]}>
+            <h2>Estadisticas</h2>
+            <span>
+              <img
+                src="https://static.wikia.nocookie.net/terraria_gamepedia/images/2/25/World_seed_icon.png/revision/latest?cb=20200921104226&format=original"
+                alt=""
+              />
+              Tu puntaje final fue: {puntaje}
+            </span>
+            <span>
+              <img
+                src="https://static.wikia.nocookie.net/terraria_gamepedia/images/0/00/World_name_icon.png/revision/latest?cb=20200921104151&format=original"
+                alt=""
+              />
+              Cantidad de palabras: {lista.length}
+            </span>
+          </div>
+          <Link to={"/"} className={style["btn-reiniciar"]}>
+            Otra partida
+          </Link>
+        </section>
         <aside className={style["lista-palabras"]}>
           <ul>
             {lista.map((palabra: string, index: number) => (
